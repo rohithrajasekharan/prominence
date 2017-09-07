a80eb3220fe5a6ece169347a7f3bcc6909996720
rohithrajasekharan <rohithrajasekharan2015@gmail.com>
2017-09-07 11:40:18 +0530

    Adding node modules and setting up entry point

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+var express=require('express');
+var router=express.Router();
+//Get Register
+router.get('/register',function(req,res){
+  res.render('register');
+});
+router.get('/login',function(req,res){
+  res.render('login');
+});
+module.exports=router;
--- server.js
+++ server.js
@@ -1,47 +1,46 @@
-var express = require('express'),
-app = express();
+var express=require("express");
+var path=require("path");
+var cookieParse=require("cookie-parser");
+var bodyParser=require("body-parser");
+var exphbs=require('express-handlebars');
+var flash=require("flash");
+var session=require("express-session");
+var passport=require("passport");
+var localStrategy=require("passport-local").Strategy;
+var mongo=require("mongodb");
+var mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost/prominence");
+var db = mongoose.connection;
+var routes=require("./routes/index");
+var users=require("./routes/users");
+var app=express();
+app.set('views', path.join(__dirname, "views"));
+app.engine("handlebars",exphbs({defaultLayout:"layout"}));
+app.set('view engine', 'handlebars');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+	secret:"secret",
+	saveUninitialized: true,
+	resave: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-var userService = require('./user_service');
+//flash messages
+app.use(flash());
 
-//Create new user
-app.post('/user', function(req, res) {
-
-	var newUserEmail = req.headers['user-email'];
-	var newUserPass = req.headers['user-pass'];
-
-	userService.addUser(newUserEmail, newUserPass,
-
-		function(error, uid) {
-
-			if (error) {
-				return res.status(500).send('Error when creating user');
-
-			} else {
-				return res.status(201).send({uid : uid});
-		}
-	});
+app.use(function(req, res, next){
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
 });
+app.use('/', routes);
+app.use('/users',users);
 
-//Perform authentication
-app.post('/login', function(req, res){
-
-	var userEmail = req.headers['user-email'];
-	var userPassword = req.headers['user-pass'];
-
-	userService.authenticate(userEmail, userPassword,
-
-		function(error, authData) {
-
-			if (error) {
-				return res.status(401).send('Unauthorized');
-
-			} else {
-				return res.status(200).send(authData);
-		}
-
-	});
-});
-
-app.listen(process.env.PORT || 3000, function(){
-	console.log('Server started...');
+app.set('port', (process.env.PORT || 3000));
+app.listen(app.get('port'), function(){
+	console.log('Server started on port'+app.get('port'));
 });
